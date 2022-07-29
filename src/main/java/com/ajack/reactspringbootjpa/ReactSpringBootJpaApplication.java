c5e1e528f533d4abdd947d456df3edd89840429f
Atticus Jack <atticusjack@gmail.com>
2022-07-29 15:01:43 -0400

    initial commit, code from IntelliJ project creation

---
+++ src/main/java/com/ajack/reactspringbootjpa/ReactSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ajack.reactspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringBootJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajack/reactspringbootjpa/ReactSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajack.reactspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
