cf445284ef277e7589fa87ac13935b60edf604e0
Atticus Jack <atticusjack@gmail.com>
2022-07-30 12:33:42 -0400

    updating schema to auto generate id's, entitys have already been changed to reflect this

---
+++ src/main/resources/db/migration/V1__initial_database_schema.sql
@@ -0,0 +1,43 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS accounts(
+    id UUID NOT NULL DEFAULT uuid_generate_v1(),
+    name varchar(200),
+    CONSTRAINT pk_accounts
+        PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    id UUID NOT NULL DEFAULT uuid_generate_v1(),
+    account_id UUID NOT NULL,
+    first_name varchar(20),
+    hid varchar(20) NOT NULL,
+    last_name varchar(20),
+    CONSTRAINT pk_users
+        PRIMARY KEY (id),
+    CONSTRAINT fk_users_accounts
+        FOREIGN KEY (account_id)
+        REFERENCES accounts (id)
+);
+
+CREATE TABLE IF NOT EXISTS organizations(
+    id UUID NOT NULL DEFAULT uuid_generate_v1(),
+    account_id UUID NOT NULL,
+    name varchar(100),
+    CONSTRAINT pk_organizations
+        PRIMARY KEY (id),
+    CONSTRAINT fk_organizations_accounts
+        FOREIGN KEY (account_id)
+        REFERENCES accounts (id)
+);
+
+CREATE TABLE IF NOT EXISTS reports(
+    id UUID NOT NULL DEFAULT uuid_generate_v1(),
+    organization_id UUID NOT NULL,
+    name varchar(100),
+    CONSTRAINT pk_reports
+        PRIMARY KEY (id),
+    CONSTRAINT fk_reports_organizations
+        FOREIGN KEY (organization_id)
+        REFERENCES organizations (id)
+);
