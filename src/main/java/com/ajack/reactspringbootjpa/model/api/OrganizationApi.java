4610550898b0f6649e40041b660fe776660196d1
Atticus Jack <atticusjack@gmail.com>
2022-07-31 11:43:33 -0400

    updating OrganizationApi to have customer and policy numbers

--- src/main/java/com/ajack/reactspringbootjpa/model/api/OrganizationApi.java
+++ src/main/java/com/ajack/reactspringbootjpa/model/api/OrganizationApi.java
@@ -14,5 +14,7 @@ import java.util.UUID;
 public class OrganizationApi
 {
     private UUID id;
-    private String name;
+    private String customerNumber;
+    private String ein;
+    private String policyNumber;
 }
