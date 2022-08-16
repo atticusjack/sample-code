1f9271b2b8399e6e9c48063b883ed1e06053a0cf
Atticus Jack <atticusjack@gmail.com>
2022-08-16 16:49:24 -0400

    Adding Cross Origin annotation to allow for local developemnt

--- src/main/java/com/ajack/reactspringbootjpa/controller/OrganizationController.java
+++ src/main/java/com/ajack/reactspringbootjpa/controller/OrganizationController.java
@@ -4,12 +4,14 @@ import com.ajack.reactspringbootjpa.model.api.OrganizationApi;
 import com.ajack.reactspringbootjpa.service.OrganizationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 @Slf4j
--- src/main/java/com/ajack/reactspringbootjpa/controller/UserController.java
+++ src/main/java/com/ajack/reactspringbootjpa/controller/UserController.java
@@ -4,10 +4,12 @@ import com.ajack.reactspringbootjpa.model.api.UserApi;
 import com.ajack.reactspringbootjpa.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 @Slf4j
